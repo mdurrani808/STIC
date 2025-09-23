49a3432639bdfab5eba858d6fd9e6107fe00ad64
Chris Kauffman <kauffman77@gmail.com>
2025-09-23 17:09:40 -0400

    Mods by Kauffman, fixes to todo.sh and added test cases

--- documentation/src/ApplicationDays/todo.sh
+++ documentation/src/ApplicationDays/todo.sh
@@ -3,34 +3,47 @@
 
 TODO_FILE="$HOME/.todos"
 
-add_todo() {
+function add_todo() {           # with 'function'
     echo "[ ] $*" >> "$TODO_FILE"
     echo "Added: $*"
 }
 
-show_todos() {
+show_todos() {                  # or without is the same
     cat -n "$TODO_FILE" | sed 's/\[ \]/ ⬜/g' | sed 's/\[x\]/ ✅/g'
 }
 
 complete_todo() {
     for num in "$@"; do
-        sed -i "" "${num}s/\[ \]/[x]/" "$TODO_FILE"
+        # works on Linux; no parameter after -i
+        sed -i "${num}s/\[ \]/[x]/" "$TODO_FILE"
+        # # failed on Linux as pattern was taken as the file name
+        # sed -i "" "${num}s/\[ \]/[x]/" "$TODO_FILE"
         echo "Completed task #$num"
     done
 }
 
 cleanup() {
     local count=$(grep -c "\[x\]" "$TODO_FILE")
-    grep -v "\[x\]" "$TODO_FILE" > temp && mv temp "$TODO_FILE"
+
+    # # Original which fails on removing all items
+    # grep -v "\[x\]" "$TODO_FILE" > temp && mv temp "$TODO_FILE"
+
+    # corrected which ALWAYS moves the temp file 
+    grep -v "\[x\]" "$TODO_FILE" > temp
+    mv temp "$TODO_FILE"
     echo "Cleaned up $count completed tasks"
 }
 
 remove_todo() {
-    sed -i "" "${1}d" "$TODO_FILE"
+    sed -i "${1}d" "$TODO_FILE"
+    # sed -i "" "${1}d" "$TODO_FILE"
     echo "Removed task #$1"
 }
 
-touch $TODO_FILE
+# only create the file if it isn't present
+if [[ ! -e $TODO_FILE ]]; then
+    touch $TODO_FILE
+fi
 
 case "$1" in
     add)    shift; add_todo "$@" ;;
